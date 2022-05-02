534a2ed073b5ff3ce0f00ce57914be5f07f957d2
Akash <akash300383@gmail.com>
2022-05-02 21:30:24 +0530

    done

--- UI/Angular Projects/angular-forms/src/app/mdf-login-page/mdf-login-page.component.ts
+++ UI/Angular Projects/angular-forms/src/app/mdf-login-page/mdf-login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-mdf-login-page',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mdf-login-page.component.css']
 })
 export class MdfLoginPageComponent implements OnInit {
-
+  
+  loginRef  = new FormGroup({
+    email:new FormControl(),
+    pass:new FormControl()
+  });
+  msg:string ="";
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  checkUser() {
+    let login = this.loginRef.value;
+    if(login.email=="Raj@gmail.com" && login.pass=="123"){
+      this.msg = "Successfull login";
+    }else {
+        this.msg  = "Failure try once again";
+    }
+  }
 }
