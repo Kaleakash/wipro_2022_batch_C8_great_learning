e415509f6bb8a71cfa693a18ac389e85087b4a4b
Akash <akash300383@gmail.com>
2022-05-05 21:28:52 +0530

    done

--- UI/Angular Projects/angular-service/src/app/product.service.ts
+++ UI/Angular Projects/angular-service/src/app/product.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from './product';
 @Injectable({
   providedIn: 'root'  // it is equal to providers attribute in app.module.ts file to register the service class. 
 })
@@ -8,9 +10,14 @@ export class ProductService {
   constructor(public http:HttpClient) { }     // DI for HttpClient pre defined API. 
 
 
-  loadProductData() {
-    this.http.get("https://fakestoreapi.com/products").
-    subscribe(result=>console.log(result),error=>console.log(error),
-    ()=>console.log("done"));
+  // loadProductData() {
+  //   this.http.get("https://fakestoreapi.com/products").
+  //   subscribe(result=>console.log(result),error=>console.log(error),
+  //   ()=>console.log("done"));
+  // }
+// the URL provide the data in json format we convert these data in product of array. 
+// we will call subscribe in component. 
+  loadProductData():Observable<Product[]>{
+    return this.http.get<Product[]>("https://fakestoreapi.com/products");
   }
 }
--- UI/Angular Projects/angular-service/src/app/product.ts
+++ UI/Angular Projects/angular-service/src/app/product.ts
@@ -1,2 +1,21 @@
+// it can be interface or class which help to map the json 
+// data retreive from backend technologies. 
 export interface Product {
+    id:number;
+    title:string;
+    price:number;
+    description:string;
+    category:string;
+    image:string;
+    rating:Rating
 }
+interface Rating {
+    rate:number;
+    count:number;
+}
+
+/*
+export class Product {
+    constructor(public id:number,public title:string.....................){}
+}
+*/
