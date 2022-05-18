33604977256938648aec970c76146c886946f2ff
Akash <akash300383@gmail.com>
2022-05-18 21:27:30 +0530

    done

---
+++ Backend Technology/Spring Framework/SpringJDBC/src/main/java/com/main/DemoTest.java
@@ -0,0 +1,30 @@
+package com.main;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bean.Employee;
+import com.service.EmployeeService;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+	
+		
+		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		EmployeeService es = (EmployeeService)ac.getBean("employeeService");
+					
+//		List<Employee> listOfEmp = es.getAllEmployee();
+//		listOfEmp.forEach(emp->System.out.println(emp));
+		
+		Employee emp = (Employee)ac.getBean("employee");
+		emp.setId(102);
+		emp.setName("Balaji");
+		emp.setSalary(45000);
+		String result  = es.storeEmployee(emp);
+		System.out.println(result);
+	}
+
+}
